890ef2da379d51c68e26840077111e43ef7d87d3
= <=>
2023-10-17 15:00:24 +0300

    npm dev

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-const path = require("path");
-const fs = require("fs");
-const { log } = require("console");
-// console.log("Название файла: ", path.basename(__filename));
-// console.log("Путь: ", path.dirname(__filename));
-// console.log("Расширение: ", path.extname(__filename));
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) throw err;
-//   console.log("Папка создана");
-// });
+import express from "express";
+const app = express();
 
-const filePath = path.join(__dirname, "tmp", "2.txt");
-console.log(filePath);
+const port = 3000;
 
-// fs.writeFile(filePath, "Something wrong in you file", function (err) {
-//   if (err) throw err;
-//   console.log("Файл создан");
-// });
-
-fs.appendFile(filePath, "tmp", function (err) {
-  if (err) throw err;
-  console.log("Файл изменён");
+app.get("/as", function (req, res) {
+  res.send("Hello students!");
 });
 
-fs.readFile(filePath, "UTF-8", (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
+app.listen(port, () => {
+  console.log("listerning on", port, "port");
 });
