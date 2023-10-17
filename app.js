95f475475c992d3a91003228e757f4c090bf9952
= <=>
2023-10-17 14:07:00 +0300

    1.2.1.2.5

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-1;
+const path = require("path");
+const fs = require("fs");
+const { log } = require("console");
+// console.log("Название файла: ", path.basename(__filename));
+// console.log("Путь: ", path.dirname(__filename));
+// console.log("Расширение: ", path.extname(__filename));
+// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
+//   if (err) throw err;
+//   console.log("Папка создана");
+// });
+
+const filePath = path.join(__dirname, "tmp", "2.txt");
+console.log(filePath);
+
+// fs.writeFile(filePath, "Something wrong in you file", function (err) {
+//   if (err) throw err;
+//   console.log("Файл создан");
+// });
+
+fs.appendFile(filePath, "tmp", function (err) {
+  if (err) throw err;
+  console.log("Файл изменён");
+});
+
+fs.readFile(filePath, "UTF-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
+});
