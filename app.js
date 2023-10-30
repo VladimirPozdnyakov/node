e082857074763eb291d85cb21bd200162d7c3b0b
= <=>
2023-10-30 10:09:11 +0300

    1

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import express from "express";
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+console.log(__dirname + "/public/favicon.png");
+
+app.use(favicon(__dirname + "/public/favicon.png"));
 
 const port = 3000;
 
 app.get("/as", function (req, res) {
-  res.send("Hello students!");
+  res.sendFile(__dirname + "/public/main.html");
+});
+app.post("/as", function (req, res) {
+  console.log(req.body);
 });
 
 // npm run dev
