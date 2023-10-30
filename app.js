89ad1e465ea795582e9d8029e3a4b63897043f31
= <=>
2023-10-30 15:02:37 +0300

    add logs

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-const express = require("express");
-const favicon = require("express-favicon");
-const path = require("path");
+import express from "express";
+import favicon from "express-favicon";
+import { join } from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+const port = "3000";
+const currentTime = new Date().toLocaleString();
+const content = `${currentTime} : Логгируем ping по адресу: localhost:${port}`;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(join(__dirname, "public")));
+app.use(favicon(join(__dirname, "/public/favicon.png")));
 
-console.log(__dirname + "/public/favicon.png");
+app.listen(port, () => {
+  fs.access("logs/logs.txt", fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.writeFile("logs/logs.txt", content, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      fs.appendFile("logs/logs.txt", `\n${content}`, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+});
 
-app.use(favicon(__dirname + "/public/favicon.png"));
-
-const port = 3000;
-
-app.get("/as", function (req, res) {
+app.get("/as", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
 });
-app.post("/as", function (req, res) {
-  console.log(req.body);
+
+app.get("/test", (req, res) => {
+  res.end("deus (dea is better) ex machina");
 });
 
-// npm run dev
-app.listen(port, () => {
-  console.log("listerning on", port, "port");
+app.post("/as", (req, res) => {
+  console.log("Проверка post пройдена");
+  console.log(req.body);
+  res.end("! !.. !!! ? ?.. ??? ?!");
 });
