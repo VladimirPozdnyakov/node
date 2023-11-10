81826f7cbf391c40ea1188bc14b2af4849077fea
= <=>
2023-11-10 15:22:48 +0300

    add ejs

--- app.js
+++ app.js
@@ -4,38 +4,41 @@ import { join } from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import ejs from "ejs";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = "3000";
+const port = 3000;
 const currentTime = new Date().toLocaleString();
-const content = `${currentTime} : Логгируем ping по адресу: localhost:${port}`;
+
+app.set("view engine", "ejs");
+app.set("\view", __dirname + "views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(join(__dirname, "public")));
 app.use(favicon(join(__dirname, "/public/favicon.png")));
 
-app.listen(port, () => {
-  fs.access("logs/logs.txt", fs.constants.F_OK, (err) => {
-    if (err) {
-      fs.writeFile("logs/logs.txt", content, (err) => {
-        if (err) throw err;
-      });
-    } else {
-      fs.appendFile("logs/logs.txt", `\n${content}`, (err) => {
-        if (err) throw err;
-      });
-    }
+function addLine(line) {
+  line = `${currentTime} :: Логгируем ping по адресу: localhost:3000` + line;
+  fs.appendFile(__dirname + "/logs/logger.txt", line + "\n", function (err) {
+    if (err) console.log(err);
   });
+}
+
+app.get("/", (req, res) => {
+  res.end("dea ex machina");
+  addLine("/");
 });
 
 app.get("/as", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
+  addLine("/as");
 });
 
 app.get("/test", (req, res) => {
   res.end("deus (dea is better) ex machina");
+  addLine("/test");
 });
 
 app.post("/as", (req, res) => {
@@ -43,3 +46,40 @@ app.post("/as", (req, res) => {
   console.log(req.body);
   res.end("! !.. !!! ? ?.. ??? ?!");
 });
+
+app.listen(port, () => {
+  addLine(" Server started");
+});
+
+// * error handler
+app.use((req, res, next) => {
+  const err = new Error("asd");
+  err.status = 404;
+  console.log(err);
+  next(err);
+});
+
+// * production error handler
+app.get("env") == "production";
+console.log(app.get("env"));
+if (app.get("env") == "production") {
+  app.use((err, req, res) => {
+    res.status(err.status);
+    res.send(err.message);
+  });
+}
+
+if (app.get("env") != "development") {
+  app.use((err, req, res, next) => {
+    res.status = 404;
+    res.render("error.ejs", err);
+  });
+} else {
+  app.use((err, req, res, next) => {
+    res.status = 404;
+    console.log("Development");
+    console.log(res.status + " Not Found");
+    console.log("Твой код — U+1FA7C");
+    res.end("SHIT CODE - U+1FA7C");
+  });
+}
