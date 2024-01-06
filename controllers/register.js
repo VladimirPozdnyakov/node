bf4446d9ddd7fba18a1fa1c404e12f302df5a35a
Vladimir Pozdnyakov <pozdnyakov044@gmail.com>
2024-01-06 18:18:21 +0300

    add email validation in register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import validatePassword from "../middleware/passwordValidation.js";
+import validator from "validator";
 
 const form = (req, res) => {
   res.render("registerForm", {
@@ -13,11 +14,19 @@ const form = (req, res) => {
 const submit = [
   validatePassword,
   (req, res, next) => {
+    const email = req.body.email;
+    if (!validator.isEmail(email) || !/@(mail\.ru|yandex\.ru)$/.test(email)) {
+      res.locals.errorMessage.push(
+        "Принимаются электронные почты только mail.ru и yandex.ru"
+      );
+      return form(req, res);
+    }
+
     if (res.locals.errorMessage && res.locals.errorMessage.length > 0) {
       return form(req, res);
     }
 
-    User.findByEmail(req.body.email, (err, user) => {
+    User.findByEmail(email, (err, user) => {
       if (err) return next(err);
       if (!user) {
         User.create(req.body, (err) => {
